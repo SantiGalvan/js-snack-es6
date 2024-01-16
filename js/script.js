c3f7e7cdae1d281d3dc31ddd1b08dec1481125fd
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-01-16 17:02:18 +0100

    add snack 1

--- js/script.js
+++ js/script.js
@@ -1,1 +1,41 @@
 console.log('JS OK');
+
+//? SNACK 1
+
+// Array degli invitati
+const guests = [
+    'Brad Pitt',
+    'Johnny Depp',
+    'Lady Gaga',
+    'Cristiano Ronaldo',
+    'Georgina Rodriguez',
+    'Chiara Ferragni',
+    'Fedez',
+    'George Clooney',
+    'Amal Clooney',
+    'Maneskin'
+];
+
+// Nome del tavolo
+const tableName = 'Tavolo Vip';
+
+// Creo l'array vuoto
+guestsList = [];
+
+// Creo un forEach
+guests.forEach((guest, i) => {
+
+    // Creo i miei oggetti
+    const newList = {
+        tableName: tableName,
+        name: guest,
+        place: i + 1,
+    };
+
+    // Pusho gli oggetti creati
+    guestsList.push(newList);
+});
+
+// Stampo in console
+console.log(guestsList);
+
