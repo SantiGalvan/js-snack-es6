034db52e1764c3d569930f7c836ad86e2125270e
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-01-16 17:23:00 +0100

    add snack 2

--- js/script.js
+++ js/script.js
@@ -39,3 +39,56 @@ guests.forEach((guest, i) => {
 // Stampo in console
 console.log(guestsList);
 
+
+//? SNACK 2
+// Array degli studenti
+const students = [
+    {
+        id: 213,
+        name: 'Marco della Rovere',
+        grades: 78,
+    },
+    {
+        id: 110,
+        name: 'Paola Cortellessa',
+        grades: 96,
+    },
+    {
+        id: 250,
+        name: 'Andrea Mantegna',
+        grades: 48,
+    },
+    {
+        id: 145,
+        name: 'Gaia Borromini',
+        grades: 74,
+    },
+    {
+        id: 196,
+        name: 'Luigi Grimaldello',
+        grades: 68,
+    },
+    {
+        id: 102,
+        name: 'Piero della Francesca',
+        grades: 50,
+    },
+    {
+        id: 120,
+        name: 'Francesca da Polenta',
+        grades: 84,
+    },
+];
+
+// - 1 Creo la lista con gli studenti che hanno un voto superiore al 70
+const topStudents = students.filter(({ grades }) => grades >= 70);
+console.log(topStudents);
+
+// - 2 Creo la lista con gli studenti che hanno un voto superiore al 70 e un id superiore a 120
+const bestStudents = students.filter(({ grades, id }) => grades >= 70 && id >= 120);
+console.log(bestStudents);
+
+// - 3 Creo una lista con i nomi degli studenti tutto in maiuscolo
+const nameStudents = []
+students.filter(({ name }) => nameStudents.push(name.toUpperCase()));
+console.log(nameStudents);
